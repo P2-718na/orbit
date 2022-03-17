b43c68ba7a5e8e6ebc913e5cd29e4882df524faf
Matteo Bonacini <matteo.bonna@gmail.com>
2022-03-17 18:51:31 +0100

    Add working camera-controls implementation

--- src/index.js
+++ src/index.js
@@ -1,35 +1,45 @@
-var THREE = require('three');
+import * as THREE from 'three';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import CameraControls from 'camera-controls';
+CameraControls.install( { THREE: THREE } );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight ); document.body.appendChild( renderer.domElement );
+const width  = window.innerWidth;
+const height = window.innerHeight;
+const clock = new THREE.Clock();
+const scene = new THREE.Scene();
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+document.body.appendChild( renderer.domElement );
 
-/*var spotLight = new THREE.SpotLight( 0xffffff );
-spotLight.position.set( 100, 1000, 100 );
-spotLight.castShadow = true;
-spotLight.shadowMapWidth = 1024;
-spotLight.shadowMapHeight = 1024;
-spotLight.shadowCameraNear = 500;
-spotLight.shadowCameraFar = 4000;
-spotLight.shadowCameraFov = 30;
+const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
 
-scene.add( spotLight );*/
-scene.add( cube );
-camera.position.z = 5;
+// This is required otherwise camera-controls don't work
+camera.position.set(0, 0, 5);
+
+const cameraControls = new CameraControls( camera, renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add(cube);
+
+const gridHelper = new THREE.GridHelper( 50, 50 );
+gridHelper.position.y = 0;
+scene.add(gridHelper);
 
 function render() {
   requestAnimationFrame( render );
 
+  const delta = clock.getDelta();
+  const elapsed = clock.getElapsedTime();
+  const updated = cameraControls.update( delta );
+
   cube.rotation.x += 0.05;
-  cube.rotation.y += 0.05;
+  //cube.rotation.y += 0.05;
 
   renderer.render( scene, camera );
 }
-
 render();
+
+
