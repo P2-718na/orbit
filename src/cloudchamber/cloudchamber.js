62609b1141e0e3a519aa73fb8884be1af25ec42b
Matteo Bonacini <matteo.bonna@gmail.com>
2022-03-31 16:24:23 +0200

    Move point cloud to its own class

---
+++ src/cloudchamber/cloudchamber.js
@@ -0,0 +1,59 @@
+import * as THREE from "three";
+
+export default class Cloudchamber {
+
+// public
+  particles;
+
+  constructor(edgeLength, vertexCount = 1E5, { pointCloudParameters, vertexSpeed }) {
+    // Setup parameters
+    this.vertexSpeed = vertexSpeed
+
+    this.geometry = new THREE.BufferGeometry();
+    this.vertices = [];
+
+    // Set initial vertex positions
+    for (let i = 0; i !== vertexCount; ++i) {
+
+      // todo add randfloat function
+      const x = edgeLength * Math.random() - edgeLength / 2;
+      const y = Math.random();
+      const z = edgeLength * Math.random() - edgeLength / 2;
+
+      this.vertices.push(x, y, z);
+    }
+
+    this.#updatePosition();
+
+    // Generate material from configuration
+    this.material = new THREE.PointsMaterial(
+      pointCloudParameters
+    );
+    this.material.color.setRGB(230, 230, 255); // todo maybe move this in config json if possible?
+
+    this.particles = new THREE.Points(this.geometry, this.material);
+  }
+
+  loop(dt) {
+    this.vertices = this.vertices.map((e, i) => {
+      if ((i + 2) % 3 === 0) {
+        const nextPos = (e - this.vertexSpeed * dt)
+        return nextPos < 0 ? nextPos + 1 : nextPos; //todo do this with a modulo pls
+      }
+
+      return e
+    })
+
+    this.#updatePosition()
+  }
+
+// Private
+  #updatePosition() {
+    this.geometry.setAttribute(
+      'position',
+      new THREE.Float32BufferAttribute(
+        this.vertices, 3
+      )
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import Cloudchamber from "./cloudchamber/cloudchamber.js"
 
 import CameraControls from 'camera-controls';
 CameraControls.install( { THREE: THREE } );
@@ -23,50 +24,25 @@ const gridHelper = new THREE.GridHelper( 50, 10 );
 gridHelper.position.y = 0;
 scene.add(gridHelper);
 
-const geometry = new THREE.BufferGeometry();
-let vertices = [];
+const cc = new Cloudchamber(50,
+  3E4,
+  {
+    pointCloudParameters: require("../config/point-cloud-white.json"),
+    vertexSpeed: 0.1,
+  });
 
-for ( let i = 0; i < 10000; i ++ ) {
-
-  const x = 50 * Math.random() - 25;
-  const y = Math.random();
-  const z = 50*Math.random() -25;
-
-  vertices.push( x, y, z );
-
-}
-
-geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-
-const material = new THREE.PointsMaterial( { size: 1, sizeAttenuation: false, alphaTest: 0.5, transparent: true } );
-material.color.setRGB( 230, 230, 255);
-
-const particles = new THREE.Points( geometry, material );
-scene.add( particles );
+scene.add(cc.particles);
 
 function render() {
-  requestAnimationFrame( render );
+  requestAnimationFrame(render);
 
   const delta = clock.getDelta();
   const elapsed = clock.getElapsedTime();
-  const updated = cameraControls.update( delta );
+  const updated = cameraControls.update(delta);
 
-  //cube.rotation.y += 0.05;
+  cc.loop(delta);
 
-  renderer.render( scene, camera );
-
-
-  // Move particles:
-  vertices = vertices.map((e, i) => {
-    if ((i + 2) % 3 === 0) {
-      const nextPos = (e - 0.1 * delta)
-      return nextPos < 0 ? nextPos + 1 : nextPos; //todo do this with a modulo pls
-    }
-
-    return e
-  })
-
-  geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+  renderer.render(scene, camera);
 }
 render();
 
