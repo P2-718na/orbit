d99b68d3d026bd02a4c7ae4a6dd3641370fb056c
Matteo Bonacini <matteo.bonna@gmail.com>
2022-03-31 15:40:49 +0200

    Add point cloud

--- src/index.js
+++ src/index.js
@@ -19,14 +19,30 @@ camera.position.set(0, 0, 5);
 
 const cameraControls = new CameraControls( camera, renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add(cube);
-
-const gridHelper = new THREE.GridHelper( 50, 50 );
+const gridHelper = new THREE.GridHelper( 50, 10 );
 gridHelper.position.y = 0;
 scene.add(gridHelper);
+
+const geometry = new THREE.BufferGeometry();
+let vertices = [];
+
+for ( let i = 0; i < 10000; i ++ ) {
+
+  const x = 50 * Math.random() - 25;
+  const y = Math.random();
+  const z = 50*Math.random() -25;
+
+  vertices.push( x, y, z );
+
+}
+
+geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+const material = new THREE.PointsMaterial( { size: 1, sizeAttenuation: false, alphaTest: 0.5, transparent: true } );
+material.color.setRGB( 230, 230, 255);
+
+const particles = new THREE.Points( geometry, material );
+scene.add( particles );
 
 function render() {
   requestAnimationFrame( render );
@@ -35,10 +51,22 @@ function render() {
   const elapsed = clock.getElapsedTime();
   const updated = cameraControls.update( delta );
 
-  cube.rotation.x += 0.05;
   //cube.rotation.y += 0.05;
 
   renderer.render( scene, camera );
+
+
+  // Move particles:
+  vertices = vertices.map((e, i) => {
+    if ((i + 2) % 3 === 0) {
+      const nextPos = (e - 0.1 * delta)
+      return nextPos < 0 ? nextPos + 1 : nextPos; //todo do this with a modulo pls
+    }
+
+    return e
+  })
+
+  geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
 }
 render();
 
